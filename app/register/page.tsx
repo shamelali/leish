9f70a2dbe8703dc5cff52f24e7decbec500ea60e
Shamel <shamel@leish.my>
2026-05-23 14:24:00 +0800

    feat: remove Google OAuth from registration page only

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -17,7 +17,7 @@ export default function RegisterPage() {
           Create an account to book artists or offer your services.
         </p>
         <div className="mt-8">
-          <SupabaseAuth defaultMode="signup" />
+          <SupabaseAuth defaultMode="signup" hideOAuth />
         </div>
       </div>
     </section>
